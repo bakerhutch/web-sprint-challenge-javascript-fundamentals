2d3f63b53809e942be64bda84fa562b6431103de
Andrew <14017822+bakerhutch@users.noreply.github.com>
2020-10-16 12:37:56 -0400

    Finished MVP

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -21,10 +21,12 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
-// let temp = displaynames.forEach(function(item,ind){
-  
-// })
-// console.log(displayNames);
+zooAnimals.forEach(function (item,ind){
+  displayNames.push(zooAnimals[ind])
+  delete displayNames.population;
+  delete displayNames.state;
+})
+console.log(displayNames);
 
 /* Request 2: .map()
 
@@ -70,7 +72,7 @@ console.log(populationTotal);
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 function consume(a, b, cb) {
-  cb(a,b)
+  return cb(a,b);
 }
 
 /* Step 2: Create several functions to callback with consume();
